a7ed6c8c17001b3298d88803c8a9c67598534f52
Natalia Costa <ncalbuq@gmail.com>
2024-09-04 16:41:49 -0300

    create NewTransactionModal

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,4 +1,5 @@
 import { HeaderContainer, HeaderContent, NewTransactionButton } from "./styles";
+import * as Dialog from "@radix-ui/react-dialog";
 
 import logo from '../../assets/logo.svg';
 
@@ -7,8 +8,25 @@ export function Header() {
         <HeaderContainer>
             <HeaderContent>
                 <img src={logo} />
-                 
-                 <NewTransactionButton>Nova Transação</NewTransactionButton>
+            
+            <Dialog.Root> {/*precisa ficar por dentro de todo context do modal, dentro do botão como do próprio modal*/}
+                <Dialog.Trigger asChild> 
+                    {/*asChild = propriedade do RadixUi muda para que o Dialog Trigger não precise criar
+                    um button, mas já aproveite o elemento button criado*/}
+                    <NewTransactionButton>Nova transação</NewTransactionButton>
+                </Dialog.Trigger>
+
+                <Dialog.Portal>
+                    {/*Portal = faz com que um conteudo dentro do Portal vá parar em outro lugar na aplicação*/}
+                    <Dialog.Overlay />
+                    <Dialog.Content>
+                        <Dialog.Title>Nova Transação</Dialog.Title>
+                        
+                        <Dialog.Close />
+                    </Dialog.Content>
+                </Dialog.Portal>
+            </Dialog.Root>
+
             </HeaderContent>
         </HeaderContainer>
             
---
+++ src/components/NewTransactionModal/index.tsx
@@ -0,0 +1,9 @@
+
+// vai contar a parte do modal em si
+export function NewTransactionModal() {
+    return (
+        <div>
+
+        </div>
+    )
+}
