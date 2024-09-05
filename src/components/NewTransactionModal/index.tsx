0b2d853510fa50477e9b71b96c20d585142fa9e2
Natalia Costa <ncalbuq@gmail.com>
2024-09-05 09:47:20 -0300

    install RadioGroup and update NewTransactionaModal

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,6 +1,6 @@
 import * as Dialog from "@radix-ui/react-dialog";
-import { CloseButton, Content, Overlay } from "./styles";
-import { X } from "phosphor-react";
+import { CloseButton, Content, Overlay, TransactionType, TransactionTypeButton } from "./styles";
+import { ArrowCircleDown, ArrowCircleUp, X } from "phosphor-react";
 
 
 // vai contar a parte do modal em si
@@ -20,6 +20,19 @@ export function NewTransactionModal() {
                 <input type="text" placeholder="Descrição" required />
                 <input type="number" placeholder="Preço" required />
                 <input type="text" placeholder="Categoria" required />
+
+                <TransactionType>
+                    <TransactionTypeButton variant="income" value="income">
+                    {/*o RadioGroup.Item obrigada a colocar o value*/}
+                      <ArrowCircleUp size={24} />
+                      Entrada
+                    </TransactionTypeButton>
+                    
+                    <TransactionTypeButton variant="outcome" value="outcome">
+                      <ArrowCircleDown size={24} />
+                      Saída
+                    </TransactionTypeButton>
+                </TransactionType>
 
                 <button type="submit">Cadastrar</button>
             </form>
