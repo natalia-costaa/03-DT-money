8e66b6f80b9e6e2533b19ae432ae001c6ebe4ab2
Natalia Costa <ncalbuq@gmail.com>
2024-10-08 15:49:50 -0300

    create seacrh transactions

--- src/pages/Transactions/components/SearchForm/index.tsx
+++ src/pages/Transactions/components/SearchForm/index.tsx
@@ -3,6 +3,8 @@ import { SearchFormContainer } from "./styles";
 import { useForm } from "react-hook-form";
 import * as z from 'zod';
 import { zodResolver } from "@hookform/resolvers/zod";
+import { useContext } from "react";
+import { TransactionContext } from "../../../../contexts/TransactionContext";
 
 const searchFormSchema = z.object({
     query: z.string(),
@@ -12,15 +14,14 @@ type SearchFormInputs = z.infer<typeof searchFormSchema>;
 
 export function SearchForm() {
 
+    const { fetchTransactions } = useContext(TransactionContext);
+
     const { register, handleSubmit, formState: { isSubmitting } } = useForm<SearchFormInputs>( {
         resolver: zodResolver(searchFormSchema),
     })
 
     async function handleSearchTransactions(data: SearchFormInputs) {
-
-        await new Promise(resolve =>setTimeout(resolve, 2000));
-
-        console.log(data)
+        await fetchTransactions(data.query);
     }
 
     return (
