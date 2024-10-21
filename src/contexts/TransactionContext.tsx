a88376d7b165c0b561a8a3cdbd523ca34ea8dc4f
Natalia Costa <ncalbuq@gmail.com>
2024-10-21 10:26:41 -0300

    installing library axios

--- src/contexts/TransactionContext.tsx
+++ src/contexts/TransactionContext.tsx
@@ -25,7 +25,7 @@ export function TransactionsProvider({ children } : TransactionProviderProps) {
     const [transactions, setTransactions] = useState<Transaction[]>([]);
 
     async function fetchTransactions(query?: string) {
-        const url = new URL('http://localhost:3000/transactions');
+        const url = new URL('/transactions');
 
         if (query) {
             url.searchParams.append('q', query);
